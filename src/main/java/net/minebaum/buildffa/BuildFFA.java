0f17bafd68ddab252c20ef43f9cd46e3a4ffb7d1
admin <admin@minebaum.net>
2021-01-05 22:04:46 +0100

    + added project structure

---
+++ src/main/java/net/minebaum/buildffa/BuildFFA.java
@@ -0,0 +1,21 @@
+package net.minebaum.buildffa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BuildFFA extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
