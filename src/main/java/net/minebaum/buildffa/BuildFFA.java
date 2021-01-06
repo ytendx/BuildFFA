f1576e131d54613cb7bb0a1ff0a141f170c5c781
admin <admin@minebaum.net>
2021-01-06 14:55:30 +0100

    + added project structure

--- src/main/java/net/minebaum/buildffa/BuildFFA.java
+++ src/main/java/net/minebaum/buildffa/BuildFFA.java
@@ -1,21 +1,35 @@
 package net.minebaum.buildffa;
 
+import net.minebaum.baumapi.BaumAPI;
+import net.minebaum.baumapi.game.Game;
+import net.minebaum.baumapi.game.Stats;
+import org.bukkit.GameMode;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BuildFFA extends JavaPlugin {
 
+    private static BuildFFA plugin;
+    private static Game g;
+
+    public static BuildFFA getPlugin() {
+        return plugin;
+    }
+
     @Override
     public void onLoad() {
-        super.onLoad();
+        this.getServer().getConsoleSender().sendMessage("[BuildFFA] Plugin Loaded!");
+        String[] teams;
+        g = new Game(BaumAPI.getPlugin(), "", "", new Stats(), 0, 50, );
+        plugin = this;
     }
 
     @Override
     public void onEnable() {
-
+        this.getServer().getConsoleSender().sendMessage("[BuildFFA] Plugin Enabled!");
     }
 
     @Override
     public void onDisable() {
-
+        this.getServer().getConsoleSender().sendMessage("[BuildFFA] Plugin Disabled!");
     }
 }
